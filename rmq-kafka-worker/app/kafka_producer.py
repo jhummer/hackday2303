344e6f6de2bced8f57fb1438bb1a253c44aa5e5e
Joakim Hummer <joakim.hummer@gmail.com>
2023-04-20 16:23:32 +0200

    configurable kafka host+port

--- rmq-kafka-worker/app/kafka_producer.py
+++ rmq-kafka-worker/app/kafka_producer.py
@@ -3,13 +3,15 @@ import avro.schema
 import avro.io
 from kafka import KafkaProducer
 
+import settings
+
 
 def json_serializer(data):
     return json.dumps(data).encode("utf-8")
 
 
 producer = KafkaProducer(
-    bootstrap_servers=["kafka:9092"],
+    bootstrap_servers=[f"{settings.KAFKA_HOST}:{settings.KAFKA_PORT}"],
     value_serializer=json_serializer
 )
 
--- rmq-kafka-worker/app/settings.py
+++ rmq-kafka-worker/app/settings.py
@@ -5,3 +5,6 @@ RMQ_USER = os.environ.get("RMQ_USER")
 RMQ_PASS = os.environ.get("RMQ_PASS")
 RMQ_PORT = os.environ.get("RMQ_PORT")
 RMQ_CONNSTR = f"amqp://{RMQ_USER}:{RMQ_PASS}@{RMQ_HOST}:{RMQ_PORT}"
+
+KAFKA_HOST = os.environ.get("KAFKA_HOST")
+KAFKA_PORT = os.environ.get("KAFKA_PORT")
